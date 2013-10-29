25e91e46d192588bc1e1061b5abb738ffc939ff0
XhmikosR <xhmikosr@users.sourceforge.net>
2013-10-29 13:15:03 +0200

    Enable JSHint's quotmark rule.
    Use single quotes consistently.

--- test/h5bp.js
+++ test/h5bp.js
@@ -373,7 +373,7 @@ describe('h5bp', function() {
 			});
 		});
 
-		it("should not advertise what kind of server we're running", function(done) {
+		it('should not advertise what kind of server we\'re running', function(done) {
 			helper.request()
 				.get('/test.html')
 				.expect(200)
@@ -1339,7 +1339,7 @@ describe('h5bp', function() {
 			});
 		});
 
-		it("should not advertise what kind of server we're running", function(done) {
+		it('should not advertise what kind of server we\'re running', function(done) {
 			helper.request()
 				.get('/test.html')
 				.expect(200)
